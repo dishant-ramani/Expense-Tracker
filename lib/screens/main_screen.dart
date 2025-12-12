cb0aff1bfdb5873890bdd9df5645630e4ada34c4
Dishant-Ramani <dramani987@rku.ac.in>
2025-12-12 16:28:18 +0530

    initial commit

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -3,11 +3,6 @@ import 'package:myapp/screens/home_screen.dart';
 import 'package:myapp/screens/budget_screen.dart';
 import 'package:myapp/screens/insights_screen.dart';
 import 'package:myapp/screens/settings_screen.dart';
-<<<<<<< HEAD
-=======
-import 'package:provider/provider.dart';
-import 'package:ultimate_bottom_navbar/ultimate_bottom_navbar.dart';
->>>>>>> 36a3720f98ff922fd9cf5ebcb4c31cfbdf164c66
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -26,18 +21,6 @@ class _MainScreenState extends State<MainScreen> {
     SettingsScreen(),
   ];
 
-  final List<IconData> _navIcons = const [
-    Icons.home,
-    Icons.account_balance_wallet,
-    Icons.insights,
-  ];
-
-  final List<String> _navTitles = const [
-    '',
-    '',
-    '',
-  ];
-
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
@@ -46,24 +29,13 @@ class _MainScreenState extends State<MainScreen> {
 
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
     const Color textColor = Color(0xFF0C0121);
-=======
-    final theme = Theme.of(context);
-    final bool isDark = theme.brightness == Brightness.dark;
-
-    final Color selectedColor =
-        isDark ? Colors.lightBlueAccent : theme.colorScheme.primary;
-    final Color unselectedColor =
-        isDark ? Colors.grey.shade400 : Colors.grey.shade600;
->>>>>>> 36a3720f98ff922fd9cf5ebcb4c31cfbdf164c66
 
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
         child: Column(
           children: [
-<<<<<<< HEAD
             // -------------------------
             // TOP HEADER (Figma exact)
             // -------------------------
@@ -95,78 +67,6 @@ class _MainScreenState extends State<MainScreen> {
                       fontSize: 20,
                       fontWeight: FontWeight.w600,
                       color: textColor,
-=======
-            // 🔹 Glassy Top Bar
-            Container(
-              margin: const EdgeInsets.fromLTRB(16, 16, 16, 12),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(24),
-                boxShadow: [
-                  BoxShadow(
-                    color: isDark
-                        ? Colors.blueAccent.withOpacity(0.25)
-                        : Colors.grey.withOpacity(0.2),
-                    blurRadius: 20,
-                    offset: const Offset(0, 8),
-                  ),
-                ],
-              ),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(24),
-                child: BackdropFilter(
-                  filter: ImageFilter.blur(sigmaX: 15, sigmaY: 15),
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 20, vertical: 10),
-                    decoration: BoxDecoration(
-                      gradient: isDark
-                          ? LinearGradient(
-                              begin: Alignment.topLeft,
-                              end: Alignment.bottomRight,
-                              colors: [
-                                Colors.black.withOpacity(0.85),
-                                Colors.blueGrey.withOpacity(0.7),
-                              ],
-                            )
-                          : LinearGradient(
-                              colors: [
-                                Colors.white.withOpacity(0.97),
-                                Colors.white.withOpacity(0.92),
-                              ],
-                            ),
-                      borderRadius: BorderRadius.circular(24),
-                      border: Border.all(
-                        color: isDark
-                            ? Colors.white.withOpacity(0.1)
-                            : Colors.grey.withOpacity(0.2),
-                        width: 1,
-                      ),
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Image.asset(
-                          'assets/logo.png',
-                          height: 35,
-                          fit: BoxFit.contain,
-                        ),
-                        IconButton(
-                          icon: Icon(
-                            Icons.settings_rounded,
-                            size: 30,
-                            color: isDark ? Colors.white : Colors.black87,
-                          ),
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) =>
-                                      const SettingsScreen()),
-                            );
-                          },
-                        ),
-                      ],
->>>>>>> 36a3720f98ff922fd9cf5ebcb4c31cfbdf164c66
                     ),
                   ),
 
@@ -192,7 +92,6 @@ class _MainScreenState extends State<MainScreen> {
         ),
       ),
 
-<<<<<<< HEAD
       // -------------------------
       // BOTTOM NAV BAR (Figma exact)
       // -------------------------
@@ -218,86 +117,6 @@ class _MainScreenState extends State<MainScreen> {
                 index: 0,
                 icon: Icons.home_rounded,
                 isSelected: _selectedIndex == 0,
-=======
-      // 🔹 Glassy Bottom Nav (Styled like Top Bar)
-      bottomNavigationBar: Consumer<ThemeProvider>(
-        builder: (context, themeProvider, child) {
-          return Container(
-            margin: const EdgeInsets.fromLTRB(16, 0, 16, 20),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(28),
-              boxShadow: [
-                BoxShadow(
-                  color: isDark
-                      ? Colors.blueAccent.withOpacity(0.25)
-                      : Colors.grey.withOpacity(0.2),
-                  blurRadius: 20,
-                  offset: const Offset(0, 8),
-                ),
-              ],
-            ),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(28),
-              child: BackdropFilter(
-                filter: ImageFilter.blur(sigmaX: 15, sigmaY: 15),
-                child: Container(
-                  decoration: BoxDecoration(
-                    gradient: isDark
-                        ? LinearGradient(
-                            begin: Alignment.topLeft,
-                            end: Alignment.bottomRight,
-                            colors: [
-                              Colors.black.withOpacity(0.85),
-                              Colors.blueGrey.withOpacity(0.7),
-                            ],
-                          )
-                        : LinearGradient(
-                            colors: [
-                              Colors.white.withOpacity(0.97),
-                              Colors.white.withOpacity(0.92),
-                            ],
-                          ),
-                    borderRadius: BorderRadius.circular(28),
-                    border: Border.all(
-                      color: isDark
-                          ? Colors.white.withOpacity(0.1)
-                          : Colors.grey.withOpacity(0.2),
-                      width: 1,
-                    ),
-                  ),
-                  child: UltimateBottomNavBar(
-                    icons: _navIcons,
-                    titles: _navTitles,
-                    currentIndex: _selectedIndex,
-                    onTap: (index) => _onItemTapped(index),
-
-                    // disable under-curve if it misaligns
-                    underCurve: false,
-                    staticCurve: false,
-                    backgroundHeight: 75,
-                    foregroundHeight: 75,
-                    navMargin: EdgeInsets.zero,
-                    backgroundBorderRadius: BorderRadius.circular(24),
-
-                    // make transparent since we handle background manually
-                    backgroundColor: Colors.transparent,
-                    foregroundColor: Colors.transparent,
-                    showForeGround: false,
-                    showForeGroundStrokeAllSide: false,
-                    showBackGroundStrokeAllSide: false,
-
-                    // icon/text appearance
-                    selectedIconColor: selectedColor,
-                    selectedIconSize: 50,
-                    unselectedIconColor: unselectedColor,
-                    unselectedIconSize: 32,
-                    selectedTextSize: 12,
-                    unselectedTextSize: 12,
-                    selectedTextColor: selectedColor,
-                    unselectedTextColor: unselectedColor,
-                  ),
-                ),
->>>>>>> 36a3720f98ff922fd9cf5ebcb4c31cfbdf164c66
               ),
 
               _buildNavItem(
